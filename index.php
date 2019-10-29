97d4bda3dbc76eab47ef447da464533c359aac56
brazius <brazius@gmail.com>
2019-10-29 16:07:35 +0200

    Uzduotis 2

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-    print 'Veikia!';
+<html>
+  <head>
+    <title>Aš valdau HTML <?php print 'ir PHP!' ?></title>
+  </head>
+  <body>
+      <h1><b>Tomas</b> - HTML <?php print 'ir PHP'?> asas</h1>
+<p> Moku kurti statinį turinį.</p>
+<p> Atidaryta: <?php print date("Y/m/d")?>
     
+  </body>
+</html>
