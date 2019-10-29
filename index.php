e9cfe91811127349973f80fb85a88d8fd8ff7cf1
brazius <brazius@gmail.com>
2019-10-29 17:39:19 +0200

    write comments v4

--- index.php
+++ index.php
@@ -5,10 +5,8 @@
   <body>
       <h1>Dainu tekstai</h1>
       <h2>Paragrafo Daina</h2>
-      <p>
-          Aš toks vienišas jaunas pragrafas...</br>
-          MIndau svetimo kodo gatves...
-      </p>
+      <p> Aš toks vienišas jaunas pragrafas...</p>
+      <!--<p> MIndau svetimo kodo gatves... </p> -->
 
   </body>
 </html>
