d6343aea622829484513f0de9bbe5eba7e1e3ff5
brazius <brazius@gmail.com>
2019-10-29 16:49:54 +0200

    stringu junginiai v3

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
           <?php print date("Y") . ' metų' ?>
           
       </h1>
-<p> Viskas prasidėjo <?php print date("m") . ' mėnesio, ' . date("l") .
+<p> Viskas prasidėjo <?php print date("m") . ' mėnesio, ' . date("d") .
         ' dieną' ?></p>
 
     
