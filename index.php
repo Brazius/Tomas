7e4cf80efd23110c3e7f529bd1ff80f9f5e0f7c7
brazius <brazius@gmail.com>
2019-10-30 17:16:17 +0200

    Qboom

--- index.php
+++ index.php
@@ -2,18 +2,28 @@
     <head>
         <title>Užduotis X</title>
         <style>
-            .bomb {
+            .bomb_ {
                 background-image: url('https://cdn.imgbin.com/2/8/12/imgbin-counter-strike-global-offensive-dota-2-bomb-c-4-grenade-fez4eKcEunfSmReJ0CiSRsPbJ.jpg');
                 width: <?php print date('s') ?>vh;
                 height:<?php print date('s') ?>vh;
                 background-size: cover;
                 background-position: center;
             }
+            .bomb_00 {
+                background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRV0nR2sUAGvZ35VVB7HHiyDX0J9N2BLS4xqFgzVGC-CRySVnW4');
+                width: <?php print date('1s') ?>vh;
+                height:<?php print date('1s') ?>vh;
+                background-size: cover;
+                background-position: center;
+
+            }
+
 
         </style>
     </head>
     <body>
-        <div class= "bomb">
+        <div class= "bomb_ bomb_<?php print date('s') ?>">
+            <p><?php print date('s') ?></p>
 
         </div>
 
