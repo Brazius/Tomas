b19a1aeb5b4dce0316051e4df1e60ae19079a341
brazius <brazius@gmail.com>
2019-10-30 16:42:28 +0200

    bombos laikmatis

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
         <style>
             .bomb {
                 background-image: url('https://cdn.imgbin.com/2/8/12/imgbin-counter-strike-global-offensive-dota-2-bomb-c-4-grenade-fez4eKcEunfSmReJ0CiSRsPbJ.jpg');
-                width: <?php print date('s') ?>px;
-                height:<?php print date('s') ?>px;
+                width: <?php print date('s') ?>vh;
+                height:<?php print date('s') ?>vh;
                 background-size: cover;
                 background-position: center;
             }
