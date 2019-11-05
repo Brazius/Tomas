c3a8bd21d7e85e6d58e9979a5ce8f83a5e80488d
brazius <brazius@gmail.com>
2019-11-05 12:03:49 +0200

    algos-skaiciuokle

--- index.php
+++ index.php
@@ -1,33 +1,23 @@
-
 <?php
-$kates = rand(1, 3);
-$sunys = rand(1, 3);
-$katasuniai = 0;
+$months = 12;
+$wallet = 1000;
+$month_income = 700;
 
-print "kat rand $kates </br> sun rand $sunys </br>";
-
-for ($kat_x = 1; $kat_x <= $kates; $kat_x++){
-    for ($dog_x = 1; $dog_x <= $sunys; $dog_x++){
-        $pavyko = rand(0,1);
-        echo"Kate $kat_x </br> Suo $dog_x </br>Pavyko $pavyko /// </br>";
-        if ($pavyko){
-            $katasuniai++;
-            break;
-        }
-     }
+for ($i = 1; $i <= $months; $i++) {
+    $month_expenses = rand(0, 1000);
+    $wallet += $month_income - $month_expenses;
 }
 
-print "</br></br> $katasuniai";
-
-$h1 = 'katasuniu iseiga';
-$h2 = 'Dalyvavo $Kates kates ir $sunys sunys';
-$h3 = 'katasuniai "$katasuniai"';
-?> 
-<html>
-    <body> 
+$title = 'Wallet Forecast';
+$h1 = 'Wallet Forecast';
+$h2 = "Po $months m., prognozuojamas likutis: $wallet";
+?>
+<html lang="en">
+    <head>
+        <title><?php print $title; ?></title>
+    </head>
+    <body>
         <h1><?php print $h1; ?></h1>
         <h2><?php print $h2; ?></h2>
-        <h3><?php print $h3; ?></h3>
-
     </body>
 </html>
