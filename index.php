50c597a450622b9bc4bdf246c304a46059aadb13
brazius <brazius@gmail.com>
2019-11-05 12:22:14 +0200

    ner_babkiu

--- index.php
+++ index.php
@@ -3,14 +3,19 @@ $months = 12;
 $wallet = 1000;
 $month_income = 700;
 
-for ($i = 1; $i <= $months; $i++) {
-    $month_expenses = rand(0, 1000);
-    $wallet += $month_income - $month_expenses;
-}
-
 $title = 'Wallet Forecast';
 $h1 = 'Wallet Forecast';
 $h2 = "Po $months m., prognozuojamas likutis: $wallet";
+
+for ($i = 1; $i <= $months; $i++) {
+    $month_expenses = rand(600, 1000);
+    $wallet += $month_income - $month_expenses;
+    if ($wallet <= 0) {
+        $h2 = "Atsargiai, $i menesi gali baigtis pinigai!";
+        break;
+}
+
+
 ?>
 <html lang="en">
     <head>
