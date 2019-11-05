3cfab1470777c045d977715f00b35bf2d86cc59f
brazius <brazius@gmail.com>
2019-11-05 10:32:35 +0200

    for uzduotis v1

--- index.php
+++ index.php
@@ -1,32 +1,13 @@
+
+<?php
+$x = rand(0, 20);
+
+for ($i = 1; $i <= $x; $i++) {
+    var_dump("Tai yra $i-tasis ciklas");
+}
+?> 
 <html>
-    <head>
-        <title>Užduotis X</title>
-        <style>
-            .bomb_ {
-                background-image: url('https://cdn.imgbin.com/2/8/12/imgbin-counter-strike-global-offensive-dota-2-bomb-c-4-grenade-fez4eKcEunfSmReJ0CiSRsPbJ.jpg');
-                width: <?php print date('s') ?>vh;
-                height:<?php print date('s') ?>vh;
-                background-size: cover;
-                background-position: center;
-            }
-            .bomb_00 {
-                background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRV0nR2sUAGvZ35VVB7HHiyDX0J9N2BLS4xqFgzVGC-CRySVnW4');
-                width: <?php print date('1s') ?>vh;
-                height:<?php print date('1s') ?>vh;
-                background-size: cover;
-                background-position: center;
-
-            }
-
-
-        </style>
-    </head>
-    <body>
-        <div class= "bomb_ bomb_<?php print date('s') ?>">
-            <p><?php print date('s') ?></p>
-
-        </div>
-
+    <body> 
+        
     </body>
-
 </html>
