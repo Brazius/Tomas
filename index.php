63d043f0891299de6acb272aa8f802154b94446a
brazius <brazius@gmail.com>
2019-11-05 13:26:52 +0200

    ciges

--- index.php
+++ index.php
@@ -1,21 +1,31 @@
 <?php
-$months = 12;
-$wallet = 1000;
-$month_income = 700;
+$days = 365;
 
-$title = 'Wallet Forecast';
-$h1 = 'Wallet Forecast';
-$h2 = "Po $months m., prognozuojamas likutis: $wallet";
 
-for ($i = 1; $i <= $months; $i++) {
-    $month_expenses = rand(600, 1000);
-    $wallet += $month_income - $month_expenses;
-    if ($wallet <= 0) {
-        $h2 = "Atsargiai, $i menesi gali baigtis pinigai!";
-        break;
+
+$pack_price = 3.50;
+$count_ttl = 0;
+
+for ($i = 1; $i <= $days; $i++) {
+    $w_d = date('N', strtotime("+$i days"));
+    if ($w_d == 6) {
+        $cigs_sat = rand(3, 4);
+        $count_ttl += $cigs_sat;
+    } elseif ($w_d == 7) {
+        $cigs_sun = rand(1, 3);
+        $count_ttl += $cigs_sun;
+    }
+ else {
+        $cigs_mon_fri = rand(10, 20);
+        $count_ttl += $cigs_mon_fri;
+    }
 }
 
+$price_ttl = ceil($count_ttl / 20) * $pack_price;
 
+
+$h1 = 'Mano dumu skaicius';
+$h2 = "Per $days dienas, surukysiu $count_ttl cigareciu uz $price_ttl eur.";
 ?>
 <html lang="en">
     <head>
